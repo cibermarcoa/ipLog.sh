403ac5f1365e93181af248010c8dd062e522fe3e
cibermarcoa <marcoantonio.dzcl@gmail.com>
2024-09-27 15:22:32 +0200

    test implemented

--- ipLog.sh
+++ ipLog.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# Almacena el argumento en una variable
-
-
-
 ip1=$(echo "$1" | cut -d '.' -f 1)
 ip2=$(echo "$1" | cut -d '.' -f 2)
 ip3=$(echo "$1" | cut -d '.' -f 3)
 ip4=$(echo "$1" | cut -d '.' -f 4)
-
-
-
 
 if echo "$ip1 $ip2 $ip3 $ip4" | grep -q '[a-zA-Z]'
     then
@@ -18,14 +11,12 @@ if echo "$ip1 $ip2 $ip3 $ip4" | grep -q '[a-zA-Z]'
         exit 1
 fi
 
-
-if  [ "$ip1" -ge 0 ] && [ "$ip1" -lt 256 ] &&
-    [ "$ip2" -ge 0 ] && [ "$ip2" -lt 256 ] &&
-    [ "$ip3" -ge 0 ] && [ "$ip3" -lt 256 ] &&
-    [ "$ip4" -ge 0 ] && [ "$ip4" -lt 256 ]
+if  test "$ip1" -ge 0 && test "$ip1" -lt 256 &&
+    test "$ip2" -ge 0 && test "$ip2" -lt 256 &&
+    test "$ip3" -ge 0 && test "$ip3" -lt 256 &&
+    test "$ip4" -ge 0 && test "$ip4" -lt 256
     then
         echo "Valid"
     else
         echo "Invalid"
 fi
-
