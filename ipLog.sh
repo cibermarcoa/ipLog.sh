7bede0b942a5dd227cc29a7874a914abeaabaca1
dtorresss <diegotorresprofesional@gmail.com>
2024-09-27 16:10:12 +0200

    Added some checkings

--- ipLog.sh
+++ ipLog.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+#!/bin/bash
+
+ip_checker() {
+    ip1=$(echo "$1" | cut -d '.' -f 1)
+    ip2=$(echo "$1" | cut -d '.' -f 2)
+    ip3=$(echo "$1" | cut -d '.' -f 3)
+    ip4=$(echo "$1" | cut -d '.' -f 4)
+
+    if echo "$ip1 $ip2 $ip3 $ip4" | grep -q '[a-zA-Z]'
+        then
+            echo "ERROR: La dirección IPv4 tiene letras"
+            exit 1
+    fi
+
+    if  test "$ip1" -ge 0 && test "$ip1" -lt 256 &&
+        test "$ip2" -ge 0 && test "$ip2" -lt 256 &&
+        test "$ip3" -ge 0 && test "$ip3" -lt 256 &&
+        test "$ip4" -ge 0 && test "$ip4" -lt 256
+        then
+            echo "Valid"
+        else
+            echo "Invalid"
+    fi
+}
+
 # EXCEDEED ARGUMENTS
 if test $# -gt 2
     then
@@ -29,32 +54,12 @@ then
 fi
 
 # DIRECOTIO AND IP
-if test 
-
-
-
-
-
-
-ip_checker() {
-    ip1=$(echo "$1" | cut -d '.' -f 1)
-    ip2=$(echo "$1" | cut -d '.' -f 2)
-    ip3=$(echo "$1" | cut -d '.' -f 3)
-    ip4=$(echo "$1" | cut -d '.' -f 4)
-
-    if echo "$ip1 $ip2 $ip3 $ip4" | grep -q '[a-zA-Z]'
-        then
-            echo "ERROR: La dirección IPv4 tiene letras"
-            exit 1
-    fi
-
-    if  test "$ip1" -ge 0 && test "$ip1" -lt 256 &&
-        test "$ip2" -ge 0 && test "$ip2" -lt 256 &&
-        test "$ip3" -ge 0 && test "$ip3" -lt 256 &&
-        test "$ip4" -ge 0 && test "$ip4" -lt 256
-        then
-            echo "Valid"
-        else
-            echo "Invalid"
-    fi
-}
+if test -d $1 && ip_checker $2
+then
+    echo $2 es una IP y $1 un directorio. Buscamos dentro:
+    logsFiles=$(find $1 -name "*.log")
+    for LOG in $logsFiles
+    do
+        
+    done
+fi
